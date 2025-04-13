c0b7debdc9bad1068b20bbeeaf2faf07740e34d0
kohdice <kohdice@gmail.com>
2025-04-14 00:40:53 +0900

    feat: Add errQuit

--- error.go
+++ error.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
+// errQuit is a sentinel error returned when the user selects Quit.
+var errQuit = errors.New("quit selected")
+
+// exitWithError prints the error message to stderr and returns a non-zero status code.
 func exitWithError(err error) int {
 	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 	return 1
