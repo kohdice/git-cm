70d48f38a9fc5c98bcbec03b8f71583348399920
kohdice <kohdice@gmail.com>
2025-04-14 12:05:29 +0900

    feat: Added version flag and called doCommit in main

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 var (
-	version  = "v0.0.1"
+	version  = "v0.1.0"
 	revision = "HEAD"
 )
 
 func main() {
-	fmt.Printf("%s (rev %s)", version, revision)
+	showVersion := flag.Bool("version", false, "Show version")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("%s (rev: %s)\n", version, revision)
+		os.Exit(0)
+	}
+
+	os.Exit(doCommit())
 }
