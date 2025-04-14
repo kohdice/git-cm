8ad913ac6ab2b209c930d8f6454a523c65fb0e80
kohdice <kohdice@gmail.com>
2025-04-14 23:37:31 +0900

    chore: Remove revision

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 	"os"
 )
 
-var (
-	version  = "v0.1.0"
-	revision = "HEAD"
-)
+var version = "v0.0.1"
 
 func main() {
 	showVersion := flag.Bool("version", false, "Show version")
 	flag.Parse()
 
 	if *showVersion {
-		fmt.Printf("%s (rev: %s)\n", version, revision)
+		fmt.Println(version)
 		os.Exit(0)
 	}
 
